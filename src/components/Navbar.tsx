c90804dbc706563db3d61d7cb8c6d0c5b09d7d42
BinhThanh313 <binhdt1303@gmail.com>
2025-05-16 23:18:06 +0700

    Animated Chart Tutorial

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
                 <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
                     <Image src="/message.png" alt='' width={20} height={20}/>
                 </div>
-                 <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+                 <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
                     <Image src="/announcement.png" alt='' width={20} height={20}/>
                     <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs">1</div>
                 </div>
