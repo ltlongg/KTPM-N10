c967d2a561ab429e26c0746cc0e2afbc6878969a
BinhThanh313 <binhdt1303@gmail.com>
2025-05-16 21:47:45 +0700

    Tailwind Responsive Navbar Design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,9 @@
 const AdminPage = () => {
   return (
-    <div className=''>AdminPage</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      <div className="w-full lg:w-2/3">l</div>
+      <div className="w-full lg:w-2/3">r</div>
+    </div>
   )
 }
 
