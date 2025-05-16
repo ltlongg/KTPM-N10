e47ae7864b3615e4f28bcaa12854bc0f533d0355
BinhThanh313 <binhdt1303@gmail.com>
2025-05-17 03:09:32 +0700

    Announcement Component Design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,10 @@
+import Announcements from '@/components/Announcements'
+import EventCalendar from '@/components/EventCalendar'
 import AttendenceChart from '@/components/AttendenceChart'
 import CountChart from '@/components/CountChart'
 import UserCard from '@/components/UserCard'
 import FinanceChart from '@/components/FinanceChart'
-import EventCalendar from '@/components/EventCalendar'
+
 
 const AdminPage = () => {
   return (
@@ -27,7 +29,11 @@ const AdminPage = () => {
       </div>
       </div>
       <div className="w-full lg:w-1/3 flex lex-col gap-8">
-        <EventCalendar />
+        <div>
+          <EventCalendar/>
+          <Announcements/>       
+        </div>
+      
       </div>
     </div>
   )
--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -36,14 +36,14 @@ const events = [
 ];
 
 const EventCalendar = () => {   
-    const [value, setValue] = useState<Value>(new Date());
+    const [selectedDate, setSelectedDate] = useState<Value>(new Date());
 
     return (
         <div className="bg-white p-4 rounded-md">
-            <Calendar onChange={setValue} value={value} />
+            <Calendar onChange={setSelectedDate} value={selectedDate} />
             <div className="flex items-center justify-between">
                 <h1 className='text-xl font-semibold my-4'>Event</h1>
-                <Image src="/.moreDark.png" alt="" width={20} height={20} />
+                <Image src="/moreDark.png" alt="" width={20} height={20} />
             </div>
             <div className="flex flex-col gap-4">
                 {events.map((event) => (
