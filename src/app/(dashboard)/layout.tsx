ce2237cbaaad5d2e27df627a1d0c477e28a105a4
BinhThanh313 <binhdt1303@gmail.com>
2025-05-16 15:35:54 +0700

    Tailwind Responsive Menu Design

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import {Menu} from "@/components/Menu";
+import {Navbar} from "@/components/Navbar";
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -12,8 +13,7 @@ export default function DashboardLayout({
   return (
     <div className = 'h-screen flex'>
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] l:w-[16%] xl:w-[14%] bg-red-50">
-        l
+      <div className="w-[14%] md:w-[8%] l:w-[16%] xl:w-[14%] ">
         <Link 
             href="/" 
             className='flex items-center justify-center lg:justify-start gap-2'
@@ -24,8 +24,9 @@ export default function DashboardLayout({
        <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[84%] md:w-[92%] l:w-[84%] xl:w-[86%] bg-blue-50">
-        r
+      <div className="w-[84%] md:w-[92%] l:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
       </div>
     </div> 
     // h-screen: chiều cao toàn màn hình
