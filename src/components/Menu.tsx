51531ab5d603958b92cdcacbf2637d424ca34e6c
BinhThanh313 <binhdt1303@gmail.com>
2025-05-17 03:23:18 +0700

    fix 3

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -115,14 +115,14 @@ const menuItems = [
     ],
   },
 ];
-export const Menu = () => {
+const Menu = () => {
   return (
     <div className="mt-4 text-sm">
       {menuItems.map((i) =>(
         <div className="flex flex-col gap-2" key={i.title}>
           <span className='hidden lg:block text-gray-400 front-light my-4'>{i.title}</span>
           {i.items.map(item=>(
-            <Link href={item.href} key = {item.label} className='flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2'>
+            <Link href={item.href} key = {item.label} className='flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 pl-4'>
               <Image src={item.icon} alt ="" width={20} height={20}/>
               <span className='hidden lg:block'>{item.label}</span>
             </Link>
@@ -132,3 +132,4 @@ export const Menu = () => {
     </div>
   )
 }
+export default Menu
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-export const Navbar = () => {
+const Navbar = () => {
     return (
         <div className="flex items-center justify-between p-4">
             <div className="hidden md:flex items-center gap-2 tex-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
