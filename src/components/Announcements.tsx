9c5f2fc87339314bbe2234f8f8d719ccb3f25ee1
ltlong <lethanhlong9a1819@gmail.com>
2025-05-18 21:27:53 +0700

    student dashboard design

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -8,7 +8,7 @@ const Announcements = () => {
             <div className="flex flex-col gap-4 mt-4">
                 <div className="bg-lamaSkyLight rounded-md p-4">
                     <div className="flex items-center justify-between">
-                        <h2>Lorem ipsum dolor sit</h2>
+                        <h2 className="font-medium">Lorem ipsum dolor sit</h2>
                         <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
                     </div>
                     <p className="text-sm text-gray-400 mt-1">
@@ -19,7 +19,7 @@ const Announcements = () => {
                 </div>
                 <div className="bg-lamaPurpleLight rounded-md p-4">
                     <div className="flex items-center justify-between">
-                        <h2>Lorem ipsum dolor sit</h2>
+                        <h2 className="font-medium">Lorem ipsum dolor sit</h2>
                         <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
                     </div>
                     <p className="text-sm text-gray-400 mt-1">
@@ -31,7 +31,7 @@ const Announcements = () => {
                 
                 <div className="bg-lamaYellowLight rounded-md p-4">
                     <div className="flex items-center justify-between">
-                        <h2>Lorem ipsum dolor sit</h2>
+                        <h2 className="font-medium">Lorem ipsum dolor sit</h2>
                         <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
                     </div>
                     <p className="text-sm text-gray-400 mt-1">
