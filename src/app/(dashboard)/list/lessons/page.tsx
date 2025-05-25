82bdcaf880ff2adab9700439ea888d370ab89554
DiunHuang736 <trunglol2606@gmail.com>
2025-05-25 17:54:51 +0700

    Update lessons

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -1,9 +1,9 @@
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import {role, lessonsData } from "@/lib/data";
+import { lessonsData, role } from "@/lib/data";
 import Image from "next/image";
-import Link from "next/link";
 
 type Lesson = {
   id: number;
@@ -15,7 +15,7 @@ type Lesson = {
 const columns = [
   {
     header: "Subject Name",
-    accessor: "subject",
+    accessor: "name",
   },
   {
     header: "Class",
@@ -31,6 +31,7 @@ const columns = [
     accessor: "action",
   },
 ];
+
 const LessonListPage = () => {
   const renderRow = (item: Lesson) => (
     <tr
@@ -42,16 +43,11 @@ const LessonListPage = () => {
       <td className="hidden md:table-cell">{item.teacher}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-              <Image src="/edit.png" alt="" width={16} height={16} />
-            </button>
-          </Link>
           {role === "admin" && (
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
-            // <FormModal table="student" type="delete" id={item.id}/>
+            <>
+              <FormModal table="lesson" type="update" data={item} />
+              <FormModal table="lesson" type="delete" id={item.id} />
+            </>
           )}
         </div>
       </td>
@@ -72,12 +68,7 @@ const LessonListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === "admin" && (
-              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-                <Image src="/plus.png" alt="" width={14} height={14} />
-              </button>
-            //   <FormModal table="student" type="create"/>
-            )}
+            {role === "admin" && <FormModal table="lesson" type="create" />}
           </div>
         </div>
       </div>
