ca49ad4f9c4d2b92226faf09503ff66e9271de82
DiunHuang736 <trunglol2606@gmail.com>
2025-05-25 18:37:09 +0700

    Update add  button for every object and more new tab

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,5 +1,5 @@
 import Announcements from "@/components/Announcements"
-import BigCalendar from "@/components/BigCalender"
+import BigCalendar from "@/components/BigCalendar"
 const ParentPage = () => {
   return (
     <div className='flex-1 p-4 flex gap-4 flex-col xl:flex-row'> 
