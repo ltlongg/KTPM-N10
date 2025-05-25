326a248495a59ba7f46c3f4712c96d98b9774437
DiunHuang736 <trunglol2606@gmail.com>
2025-05-25 17:58:23 +0700

    Update layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,7 @@
 import Menu from "@/components/Menu";
 import Navbar from "@/components/Navbar";
-import Image from 'next/image';
-import Link from 'next/link';
-
-
+import Image from "next/image";
+import Link from "next/link";
 
 export default function DashboardLayout({
   children,
@@ -11,25 +9,23 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className = 'h-screen flex'>
+    <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] l:w-[16%] xl:w-[14%] ">
-        <Link 
-            href="/" 
-            className='flex items-center justify-center lg:justify-start gap-2'
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
         >
-          <Image src="/logo.png" alt="logo" width={32} height={32}/>
-          <span className="hidden lg:block font-bold">SchooLama</span> 
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block font-bold">SchooLama</span>
         </Link>
-       <Menu />
+        <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[84%] md:w-[92%] l:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
         <Navbar />
         {children}
       </div>
-    </div> 
-    // h-screen: chiều cao toàn màn hình
-    //bg-blue: cài màu màn hình
+    </div>
   );
 }
