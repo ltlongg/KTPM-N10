c8fb88f08dd9bcc0f931ee8e93e275226806435d
DiunHuang736 <trunglol2606@gmail.com>
2025-05-25 17:54:19 +0700

    Update exams

--- src/app/(dashboard)/list/exams/page.tsx
+++ src/app/(dashboard)/list/exams/page.tsx
@@ -1,9 +1,10 @@
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import {role, examsData } from "@/lib/data";
+import { examsData, role } from "@/lib/data";
 import Image from "next/image";
-import Link from "next/link";
+
 type Exam = {
   id: number;
   subject: string;
@@ -15,7 +16,7 @@ type Exam = {
 const columns = [
   {
     header: "Subject Name",
-    accessor: "subject",
+    accessor: "name",
   },
   {
     header: "Class",
@@ -49,16 +50,11 @@ const ExamListPage = () => {
       <td className="hidden md:table-cell">{item.date}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-              <Image src="/edit.png" alt="" width={16} height={16} />
-            </button>
-          </Link>
-          {role === "admin" && (
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
-            // <FormModal table="student" type="delete" id={item.id}/>
+          {role === "admin" || role === "teacher" && (
+            <>
+              <FormModal table="exam" type="update" data={item} />
+              <FormModal table="exam" type="delete" id={item.id} />
+            </>
           )}
         </div>
       </td>
@@ -79,12 +75,7 @@ const ExamListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === "admin" && (
-              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-                <Image src="/plus.png" alt="" width={14} height={14} />
-              </button>
-            //   <FormModal table="student" type="create"/>
-            )}
+            {role === "admin" || role === "teacher" && <FormModal table="exam" type="create" />}
           </div>
         </div>
       </div>
