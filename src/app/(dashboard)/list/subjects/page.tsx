b73e585730256da511f7ecbc7579d7310578ecd4
ltlong <lethanhlong9a1819@gmail.com>
2025-06-08 12:22:06 +0700

    Fetch subjects data according to user role

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -1,36 +1,49 @@
-import FormModal from "@/components/FormModal";
+// import FormContainer from "@/components/FormContainer";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role, subjectsData } from "@/lib/data";
 import prisma from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
 import { Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
+import { auth } from "@clerk/nextjs/server";
+import FormModal from "@/components/FormModal";
 
-type SubjectList = Subject &{teachers: Teacher[]};
-const columns = [
-  {
-    header: "Subject Name",
-    accessor: "name",
-  },
-  {
-    header: "Teachers",
-    accessor: "teachers",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Actions",
-    accessor: "action",
-  },
-];
+type SubjectList = Subject & { teachers: Teacher[] };
+
+const SubjectListPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+  const { sessionClaims } =await auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+
+  const columns = [
+    {
+      header: "Subject Name",
+      accessor: "name",
+    },
+    {
+      header: "Teachers",
+      accessor: "teachers",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Actions",
+      accessor: "action",
+    },
+  ];
+
   const renderRow = (item: SubjectList) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
     >
       <td className="flex items-center gap-4 p-4">{item.name}</td>
-      <td className="hidden md:table-cell">{item.teachers.map(teacher=>teacher.name).join(",")}</td>
+      <td className="hidden md:table-cell">
+        {item.teachers.map((teacher) => teacher.name).join(",")}
+      </td>
       <td>
         <div className="flex items-center gap-2">
           {role === "admin" && (
@@ -43,47 +56,40 @@ const columns = [
       </td>
     </tr>
   );
-const SubjectListPage = async ({
-  searchParams,
-}:{
-  searchParams:{[key: string]: string| undefined}; 
-}) => {
 
-  const {page, ...queryParams} = searchParams;
+  const { page, ...queryParams } = searchParams;
 
   const p = page ? parseInt(page) : 1;
 
   // URL PARAMS CONDITION
 
-  const query: Prisma.SubjectWhereInput ={};
+  const query: Prisma.SubjectWhereInput = {};
 
-  if(queryParams){
-    for(const [key,value] of Object.entries(queryParams)){
-      if(value !== undefined)
-        switch(key){
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
           case "search":
-            query.name = {
-              contains: value,
-              mode: "insensitive",
-            };
+            query.name = { contains: value, mode: "insensitive" };
             break;
-          }
+          default:
+            break;
+        }
       }
-    } 
-  
-const [data,count] = await prisma.$transaction([
-  prisma.subject.findMany({
-    where: query,
-    include: {
-      teachers: true,
-     
-    },
-    take: ITEM_PER_PAGE, 
-    skip: ITEM_PER_PAGE *(p-1),
-    }),
-  prisma.subject.count({where : query}),
-  ])
+    }
+  }
 
+  const [data, count] = await prisma.$transaction([
+    prisma.subject.findMany({
+      where: query,
+      include: {
+        teachers: true,
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+    prisma.subject.count({ where: query }),
+  ]);
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
@@ -99,14 +105,16 @@ const [data,count] = await prisma.$transaction([
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === "admin" && <FormModal table="teacher" type="create" />}
+            {role === "admin" && (
+              <FormModal table="subject" type="create" />
+            )}
           </div>
         </div>
       </div>
       {/* LIST */}
       <Table columns={columns} renderRow={renderRow} data={data} />
       {/* PAGINATION */}
-      <Pagination page={p} count={count}  />
+      <Pagination page={p} count={count} />
     </div>
   );
 };
